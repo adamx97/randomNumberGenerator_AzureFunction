95bf6c1cc6797695c44c3605e8928ac3c22d62b6
Adam Davis <adamx97@yahoo.com>
2024-03-01 14:42:39 -0500

    Enhanced draw_random to draw a background for my blog. ( https://dynamicsoftwaremd.blogspot.com )

--- draw_random.py
+++ draw_random.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageDraw
 import random
 import rd_functions_p
 import time
+import math
 
 
 def InternalPythonFunction():
@@ -27,7 +28,7 @@ def HardwarePythonFunction():
     rdObj = rd_functions_p.RdFunctions()
     bigs = []
     start = time.time()
-    for i in range(int((512 * 512) / 64)):
+    for i in range(math.ceil((512 * 512) / 64)):
         bigs.append(rdObj.RdRand64_Retry())
     # rdObj.RdRand64_Retry(3)
     mybits = get64BitsGenerator(bigs)
@@ -45,6 +46,41 @@ def HardwarePythonFunction():
     # Save the image to a file
     im.save("random_image_hardware.png")
     print("{0:.3f} seconds for hardware function".format(end - start))
+
+
+def HardwareBlogBackground():
+    # Create a new image
+    width = 1800  # 1800
+    height = 1600  # 1600
+    squaresize = 7  # the size of each square, 1 pixel is too small for most uses
+    im = Image.new(
+        "RGB", (width, height), color=(197, 220, 233)
+    )  # color=(197, 220, 233))
+    rdObj = rd_functions_p.RdFunctions()
+    bigs = []
+    start = time.time()
+    for i in range(math.ceil((width * height) / 64)):
+        bigs.append(rdObj.RdRand64_Retry())
+    # rdObj.RdRand64_Retry(3)
+    mybits = get64BitsGenerator(bigs)
+    # Draw random white pixels
+    draw = ImageDraw.Draw(im)
+
+    for y in range(0, height, squaresize):
+        for x in range(0, width, squaresize):
+            # if random.randint(0, 1):
+            # for each bit in bigs, if the bit is 1, draw a pixel
+            pixel = (next(mybits)) & 1
+            if pixel:
+                for x1 in range(x, x + squaresize):
+                    for y1 in range(y, y + squaresize):
+                        draw.point(
+                            (x1, y1), (197, 202, 233)
+                        )  # 197,202,233 is the light blue used on the resume site.
+    end = time.time()
+    # Save the image to a file
+    im.save("random_image_blog.png")
+    print("{0:.3f} seconds for blog image function".format(end - start))
 
 
 def get64BitsGenerator(bigs):
@@ -67,5 +103,6 @@ def testGenerator():
 
 
 if __name__ == "__main__":
-    InternalPythonFunction()
-    HardwarePythonFunction()
+    # InternalPythonFunction()
+    # HardwarePythonFunction()
+    HardwareBlogBackground()

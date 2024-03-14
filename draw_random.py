c97cf8d7ed76a77e5fbc29c300625647ed41afad
Adam Davis <adamx97@yahoo.com>
2024-03-14 15:36:41 -0400

    added linkedin background

--- draw_random.py
+++ draw_random.py
@@ -83,6 +83,45 @@ def HardwareBlogBackground():
     print("{0:.3f} seconds for blog image function".format(end - start))
 
 
+def HardwareLinkedInBackground(
+    color1Tuple, color2Tuple, pixelWidth=1584, pixelHeight=396
+):
+    # Create a new image
+    width = pixelWidth  # 1800
+    height = pixelHeight  # 1600
+    squaresize = 12  # the size of each square, 1 pixel is too small for most uses
+    im = Image.new(
+        "RGB",
+        (width, height),
+        color=color1Tuple,  # color1 is the initial color, some might say background
+    )  # color=(197, 220, 233))
+    rdObj = rd_functions_p.RdFunctions()
+    bigs = []
+    start = time.time()
+    for i in range(math.ceil((width * height) / 64)):
+        bigs.append(rdObj.RdRand64_Retry())
+    # rdObj.RdRand64_Retry(3)
+    mybits = get64BitsGenerator(bigs)
+    # Draw random white pixels
+    draw = ImageDraw.Draw(im)
+
+    for y in range(0, height, squaresize):
+        for x in range(0, width, squaresize):
+            # if random.randint(0, 1):
+            # for each bit in bigs, if the bit is 1, draw a pixel
+            pixel = (next(mybits)) & 1
+            if pixel:
+                for x1 in range(x, x + squaresize):
+                    for y1 in range(y, y + squaresize):
+                        draw.point(
+                            (x1, y1), color2Tuple  # color2 is the foreground color
+                        )  # 197,202,233 is the light blue used on the resume site.
+    end = time.time()
+    # Save the image to a file
+    im.save("random_image_linkedIn.png")
+    print("{0:.3f} seconds for linkedIn image function".format(end - start))
+
+
 def get64BitsGenerator(bigs):
     while bigs:
         num = bigs.pop(0)
@@ -105,4 +144,5 @@ def testGenerator():
 if __name__ == "__main__":
     # InternalPythonFunction()
     # HardwarePythonFunction()
-    HardwareBlogBackground()
+    # HardwareBlogBackground()
+    HardwareLinkedInBackground((26, 35, 126), (197, 202, 233), 1584, 396)
